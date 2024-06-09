fd9d3bae69608b91d47c3620897fa547ff5f0e07
Felipe Souza <felipesouza0500@gmail.com>
2024-06-08 21:40:34 -0300

    up senha

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '123456',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'ativ4DB'
 };
 
