59da1c1f5b2168efec7bb7c4a0eacf50a0a4bec3
Felipe Souza <felipesouza0500@gmail.com>
2024-06-08 21:40:15 -0300

    pw update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '123456',
     database: process.env.DB_NAME || 'ativ4DB'
 };
 
